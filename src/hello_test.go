e46fb8046da1785ae6825f72fa4af0b0631267a4
kassem <omarksm09@gmail.com>
2024-07-03 09:36:03 +0300

    refactor: enhance code add test case

--- src/hello_test.go
+++ src/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("kassem")
-	want := "Hello , kassem!"
+	t.Run("Say hello to people", func(t *testing.T) {
 
+		got := Hello("kassem")
+		want := "Hello, kassem"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say hello to world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+		t.Errorf("got: %v want: %v", got, want)
 	}
 }
